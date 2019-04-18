b5d44a0dff14295fc35478c9c226461d5f29a029
Ben Marquardt <ben.marquardt00@gmail.com>
2019-04-18 17:18:02 -0600

    game.cpp

--- game.cpp
+++ game.cpp
@@ -91,15 +91,30 @@ void getTurnNumber(){
 
 void playTurn(){
 	int cardCzar = getTurnNumber() % getNumPlayers();
+	std::string chosenCard;
 	int chosenCardNum;
-	int 
+	std::vector <std::string> playedCards;
+	std::string blackCard = blackCards.dealCard();
 	std::cout << Players[cardCzar].getPlayerName() << " is the card czar. They will not play a card this turn" << std::endl;
 	for(int i = 0; i < getNumPlayers(); i++){
+
 		if(cardCzar != i){
-			std::cout << "The black card for this turn is " << blackCards.dealCard() << std::endl;
-			Players[i].printHand(); //make sure name of func is printHand
-			std::cout << "Enter the number of the card with 0 being the first card and 6 being the last" << std::endl;
-			std::cin >> chosenCardNum;
+			std::cout << "The black card for this turn is " << blackCard << std::endl;
+			Players[i].printCards(//); //make sure name of func is printHand
+			std::cout << "Enter the number of the card you would like to play with 1 being the first card" << std::endl;
+			std::cin >> chosenCardnum;
+
+			while(chosenCardNum >= 7 || chosenCardNum < 1){
+				std::cout << "Enter a new number for a card you would like to play, your last choice was invalid" << std::endl;
+				Players[i].printCards(//); //make sure name of func is printHand
+				std::cin >> chosenCardNum;
+				Players[i].removeFromHand(chosenCardNum);
+			}
+
+			playedCards.push_back()
 		}
 	}
+	std::cout << "Pass the computer to " << Players[cardCzar].getPlayerName() << "." << std::endl;
+	std::cout << "Enter the number of the winning card with 0 being the first card" << std::endl;
+
 }
