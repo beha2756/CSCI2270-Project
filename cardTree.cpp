b7ec42a4c03072853c4673e28aebc02446fd8474
Tate Rees <tate.rees@colorado.edu>
2019-04-21 09:26:24 -0600

    fixing stuff

---
+++ cardTree.cpp
@@ -0,0 +1,101 @@
+#include "cardTree.hpp"
+
+#include <fstream>
+#include <iostream>
+
+//constructor
+CardTree::CardTree(){
+    root = nullptr;
+}
+
+void deleteTree(cardNode *node){
+    if(node == nullptr) return;
+    deleteTree(node->left);
+    deleteTree(node->right);
+    delete node;
+}
+
+//destructor
+CardTree::~CardTree(){
+    deleteTree(root);
+}
+
+void printCardHelper(cardNode* node){
+    if(node == nullptr) return;
+
+    printCardHelper(node->left);
+    std::cout << "Keyword: " << node->keyword << std::endl;
+    std::cout << "     " << node->text << std::endl;
+    printCardHelper(node->right);
+}
+
+// prints all cards in alphabetical order by keyword
+void CardTree::printAllCards(){
+    printCardHelper(root);
+}
+
+
+
+cardNode* searchHelper(cardNode *node, std::string keyword){
+    if(node == nullptr || node->keyword == keyword) return node;
+
+    if(keyword < node->keyword) return searchHelper(node->left, keyword);
+
+    return searchHelper(node->right, keyword);
+}
+
+// returns a pointer to a node containing the card with the keyword
+cardNode* CardTree::searchDeck(std::string keyword){
+    return searchHelper(root, keyword);
+}
+
+cardNode* createNode(std::string text, std::string keyword){
+    cardNode *nn = new cardNode(text, keyword);
+    return nn;
+}
+cardNode* CardTree::addCardHelper(std::string text, std::string keyword, cardNode *node){
+    if(root == nullptr){
+        root = createNode(text, keyword);
+        return node;
+    }
+
+    if(keyword < node->keyword){
+        if(node->left == nullptr){
+            node->left = createNode(text, keyword);
+            return node;
+        }else{
+            return addCardHelper(text, keyword, node->left);
+        }
+    }else{
+        if(node->right == nullptr){
+            node->right = createNode(text, keyword);
+            return node;
+        }else{
+            return addCardHelper(text, keyword, node->right);
+        }
+    }
+}
+
+// add a new card to the deck
+void CardTree::addCard(std::string text, std::string keyword){
+    addCardHelper(text, keyword, root);
+}
+
+void writingHelper(std::ofstream& os, cardNode *node){
+    if(node == nullptr) return;
+
+    writingHelper(os, node->left);
+    os << node->text << "|" << node->keyword << std::endl;
+    writingHelper(os, node->right);
+}
+
+// writes the cards to a file in alphabetical order (by keyword) again in case there are new cards added
+void CardTree::writeTreeToFile(std::string filename){
+    std::ofstream outFile;
+    outFile.open(filename);
+
+    writingHelper(outFile, root);
+
+    outFile.close();
+
+}
---
+++ cardTree.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+struct cardNode {
+    std::string text;
+    std::string keyword;
+    cardNode *left;
+    cardNode *right;
+    cardNode(std::string t, std::string k){
+        text = t;
+        keyword = k;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+
+class CardTree {
+public:
+    //constructor
+    CardTree();
+    //destructor
+    ~CardTree();
+    // prints all cards in alphabetical order by keyword
+    void printAllCards();
+    // returns a pointer to a node containing the card with the keyword
+    cardNode* searchDeck(std::string keyword);
+    // add a new card to the deck
+    void addCard(std::string text, std::string keyword);
+    // writes the cards to a file again (by keyword) in case there are new cards added
+    void writeTreeToFile(std::string filename);
+private:
+    // helper function to facilitate recursion for adding cards
+    cardNode* addCardHelper(std::string text, std::string keyword, cardNode *node);
+
+    cardNode *root;
+};
